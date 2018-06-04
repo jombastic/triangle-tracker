9b8634617ef06103b8ef8c6997b785d7e7e60236
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-06-04 23:50:21 +0200

    Add front-end logic to main.js to take the input and return a type of triangle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-//import { Triangle } from './triangle.js';
+import { Triangle } from './triangle.js';
 import './styles.css';
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(function() {
+  $("form#triangle").submit(function(event) {
+    event.preventDefault();
+
+    var sideA = parseInt($("#side-a").val());
+    var sideB = parseInt($("#side-b").val());
+    var sideC = parseInt($("#side-c").val());
+
+    if (sideA && sideB && sideC) {
+      var triangle = new Triangle(sideA, sideB, sideC);
+      var triangleType = triangle.checkType();
+
+      $(".triangle-type").text(triangleType);
+    } else {
+      $(".triangle-type").text("n cicka");
+    }
+
+    $("#result").show();
+  });
+});
